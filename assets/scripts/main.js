a2a3f7db7c8ea2fc6957829e1bd8ae873b7c0602
vreminnyi <vlad.reminniy@idealogic.dev>
2024-12-04 16:42:07 +0200

    build: main script, Greeting, DOMHelper, Storage, and Encoder classes

---
+++ assets/scripts/main.js
@@ -0,0 +1,214 @@
+const HIDDEN_CLASS = 'hidden';
+
+class Encoder {
+  static encode(text) {
+    return btoa(encodeURIComponent(text));
+  }
+
+  static decode(encodedText) {
+    try {
+      return decodeURIComponent(atob(encodedText));
+    } catch (e) {
+      console.error('Decoding error!', e);
+      return null;
+    }
+  }
+}
+
+class Storage {
+  static localStorage = localStorage;
+
+  static has(key) {
+    return localStorage.getItem(key) !== null;
+  }
+
+  static get(key) {
+    let item = localStorage.getItem(key);
+
+    if (typeof item !== 'string') return item;
+
+    if (item === 'undefined') return undefined;
+
+    if (item === 'null') return null;
+
+    // Check for numbers and floats
+    if (/^'-?\d{1,}?\.?\d{1,}'$/.test(item)) return Number(item);
+
+    // Check for serialized objects and arrays
+    if (item[0] === '{' || item[0] === '[') return JSON.parse(item);
+
+    return item;
+  }
+
+  static set(key, value) {
+    if (typeof key !== 'string') {
+      throw new TypeError(
+        `localStorage: Key must be a string. (reading '${key}')`
+      );
+    }
+
+    if (typeof value === 'object' || typeof value === 'array') {
+      value = JSON.stringify(value);
+    }
+
+    localStorage.setItem(key, value);
+  }
+
+  static remove(key) {
+    localStorage.removeItem(key);
+  }
+}
+
+class DOMHelper {
+  static prependElements(parent, ...elements) {
+    elements.forEach((el) => parent.prepend(el));
+  }
+
+  static removeElements(...elements) {
+    elements.forEach((el) => el.remove());
+  }
+
+  static hideElements(...elements) {
+    elements.forEach((el) => el.classList.add(HIDDEN_CLASS));
+  }
+
+  static showElements(...elements) {
+    elements.forEach((el) => el.classList.remove(HIDDEN_CLASS));
+  }
+}
+
+class Greeting {
+  static #instance;
+  static hashParam = 'hash';
+
+  #hash = '';
+  title = '';
+  text = 'Happy Holidays!';
+
+  static get instance() {
+    if (!Greeting.#instance) {
+      Greeting.#instance = new Greeting();
+    }
+
+    return Greeting.#instance;
+  }
+
+  isViewed() {
+    return (
+      Storage.has(Greeting.hashParam) &&
+      Storage.get(Greeting.hashParam) === this.#hash
+    );
+  }
+
+  greet() {
+    this.#parseHashData();
+
+    if (this.#hash) {
+      if (this.isViewed()) return true;
+      this.#showGreeting();
+    } else {
+      this.#showGreetingForm();
+    }
+
+    return false;
+  }
+
+  #showGreetingForm() {
+    const overlayElement = document.getElementById(
+      'greeting-card-overlay'
+    );
+    const greetingForm = document.getElementById('greeting-form');
+    const greetingLink = document.getElementById('greeting-link');
+    const cardElement = document.getElementById('greeting-card');
+
+    DOMHelper.showElements(cardElement, greetingForm, overlayElement);
+
+    greetingForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.target);
+      const formProps = Object.fromEntries(formData);
+      const hash = Greeting.instance.create(
+        formProps.title,
+        formProps.text
+      );
+      const url = new URL(window.location.href);
+      url.searchParams.set(Greeting.hashParam, hash);
+
+      const openButton = document.createElement('button');
+      const copyButton = document.createElement('button');
+
+      openButton.textContent = 'Open';
+      copyButton.textContent = 'Copy';
+
+      openButton.addEventListener('click', () => {
+        window.open(url.toString(), '_blank').focus();
+      });
+
+      copyButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(url.toString());
+        alert('Copied!');
+      });
+
+      DOMHelper.removeElements(...greetingLink.childNodes);
+      DOMHelper.prependElements(greetingLink, openButton, copyButton);
+
+      DOMHelper.showElements(greetingLink);
+    });
+  }
+
+  #showGreeting() {
+    const overlayElement = document.getElementById(
+      'greeting-card-overlay'
+    );
+    const startButton = document.getElementById('start-game');
+    const cardElement = document.getElementById('greeting-card');
+
+    const titleElement = document.createElement('h1');
+    const textElement = document.createElement('p');
+
+    titleElement.textContent = this.title;
+    textElement.textContent = this.text;
+
+    cardElement.prepend(titleElement, textElement);
+    DOMHelper.showElements(cardElement, overlayElement, startButton);
+
+    startButton.addEventListener('click', () => {
+      Storage.set(Greeting.hashParam, this.#hash);
+      DOMHelper.hideElements(cardElement, overlayElement);
+      DOMHelper.removeElements(cardElement, overlayElement);
+    });
+  }
+
+  getHash() {
+    if (this.#hash.length) return this.#hash;
+
+    const url = new URL(document.location.href);
+
+    if (!url.searchParams.has(Greeting.hashParam)) return this.#hash;
+
+    return url.searchParams.get(Greeting.hashParam);
+  }
+
+  #parseHashData() {
+    try {
+      this.#hash = this.getHash();
+      const decoded = Encoder.decode(this.#hash);
+      if (!decoded) return;
+
+      const { title, text } = JSON.parse(decoded);
+      this.title = title;
+      this.text = text;
+    } catch (e) {
+      console.error('Parse hash failure!', e);
+    }
+  }
+
+  create(title, text) {
+    return Encoder.encode(JSON.stringify({ title, text }));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  Greeting.instance.greet();
+});
