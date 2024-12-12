9d2d8af786aa5a3001f0d418f9585c9631768d2b
vreminnyi <vlad.reminniy@idealogic.dev>
2024-12-12 20:13:23 +0200

    feat: remove codesandbox watermark, add responsive styles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1321,4 +1321,23 @@ document.addEventListener('DOMContentLoaded', () => {
     moneyText: document.getElementById('money'),
     helperDialog: document.getElementById('helper-dialog'),
   });
+
+  // Прибрати кнопку сервісу при перегляді сайту
+  const removeWatermark = () => {
+    const ids = [];
+    const iframes = document.body.querySelectorAll('iframe');
+    for (const iframe of iframes) {
+      if (iframe.id.startsWith('sb__open-sandbox'))
+        ids.push(iframe.id);
+    }
+    for (const id of ids) {
+      const node = document.createElement('div');
+      node.style.setProperty('display', 'none', 'important');
+      node.id = id;
+      document.getElementById(id).remove();
+      document.body.appendChild(node);
+    }
+  };
+
+  setTimeout(removeWatermark, 1000);
 });
